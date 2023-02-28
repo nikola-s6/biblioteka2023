26353568582d0a2cc75627842484fa58d1365d52
nikola-s6 <nikola.stojilkovic6@gmail.com>
2023-02-28 10:18:57 +0100

    Dodati atributi i get i set metode u klasu Knjiga

--- Biblioteka/src/biblioteka/Knjiga.java
+++ Biblioteka/src/biblioteka/Knjiga.java
@@ -2,4 +2,42 @@ package biblioteka;
 
 public class Knjiga {
 
+	private String naslov;
+	private long isbn;
+	private String autori;
+	private String izadavac;
+	private int izdanje;
+	
+	public String getNaslov() {
+		return naslov;
+	}
+	public void setNaslov(String naslov) {
+		this.naslov = naslov;
+	}
+	public long getIsbn() {
+		return isbn;
+	}
+	public void setIsbn(long isbn) {
+		this.isbn = isbn;
+	}
+	public String getAutori() {
+		return autori;
+	}
+	public void setAutori(String autori) {
+		this.autori = autori;
+	}
+	public String getIzadavac() {
+		return izadavac;
+	}
+	public void setIzadavac(String izadavac) {
+		this.izadavac = izadavac;
+	}
+	public int getIzdanje() {
+		return izdanje;
+	}
+	public void setIzdanje(int izdanje) {
+		this.izdanje = izdanje;
+	}
+	
+	
 }
